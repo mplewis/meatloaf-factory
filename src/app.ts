b4df70aa2d733db350e5bf8b41b1799593a01e37
Matt Lewis <matt@mplewis.com>
2024-10-31 02:16:47 -0600

    feat: gibberish generator

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
 import { fastify as Fastify, FastifyServerOptions } from 'fastify'
+import { generateGibberish, indexGibberish } from './gibberish'
+import corpus from '../assets/gatsby.txt?raw'
+
+function cleanString(input) {
+  let output = input.replace(/[“”]/g, '"').replace(/[‘’]/g, "'")
+  output = output.replace(/\s+/g, ' ')
+  return output.trim()
+}
+
+const LOOKBACK_RANGE = [12, 24] as const
+const CHARS = 3000
 
 export default (opts?: FastifyServerOptions) => {
   const fastify = Fastify(opts)
 
-  /* Your code here. Hello world example: */
+  const gIndex = indexGibberish(
+    LOOKBACK_RANGE,
+    cleanString(corpus + ' ' + corpus)
+  )
+  console.log(`Index size: ${Object.keys(gIndex).length}`)
+  console.log(`Index bytes: ${JSON.stringify(gIndex).length}`)
 
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+  fastify.get('/', async (_request, _reply) => {
+    return generateGibberish(gIndex, LOOKBACK_RANGE, CHARS)
   })
 
   return fastify
